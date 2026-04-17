49f2215e9a7872a1fda50a80d91e467346102d9f
Abdulaziz-btr <buterabdul@email.com>
2026-04-17 10:19:41 +0200

    Fix model exports for deployment

--- backend/src/models/index.ts
+++ backend/src/models/index.ts
@@ -1,8 +1,4 @@
 export { User, IUser } from "./User";
 export { Job, IJob, IJobRequirements } from "./Job";
-export { Applicant, IApplicant, IProfileData } from "./Applicant";
-export {
-  ScreeningResult,
-  IScreeningResult,
-  ICandidateResult,
-} from "./ScreeningResult";
+export { Applicant } from "./Applicant";
+export { ScreeningResult } from "./ScreeningResult";
